e51de966738a66b8b1ba1273edbcc2db128e3d7d
Heliosmaster <helios89@gmail.com>
2013-01-27 21:35:13 +0100

    modular style + config.ru

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './siliwe'
+run Siliwe
--- siliwe.rb
+++ siliwe.rb
@@ -1,4 +1,4 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'data_mapper'
 require 'date'
 require 'dm-validations'
@@ -7,10 +7,6 @@ require "digest/sha1"
 require 'sinatra/flash'
 require "sinatra-authentication"
 require "pry"
-
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/siliwe.db")  
-use Rack::Session::Cookie, :secret => 'superdupersecret'
-set :sinatra_authentication_view_path, Pathname(__FILE__).dirname.expand_path + "views/"
 
 class Weight  
   include DataMapper::Resource  
@@ -23,83 +19,94 @@ class Weight
   validates_within :date, :set => (Date.new(1900,1,1)..Date.today)
 end
 
-
 class DmUser
   property :name, String, :required => true, :unique => true
 end
 
-DataMapper.finalize.auto_upgrade!
+class Siliwe < Sinatra::Base
+	register Sinatra::Flash
+	DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/siliwe.db")  
+	use Rack::Session::Cookie, :secret => 'superdupersecret'
+	set :sinatra_authentication_view_path, Pathname(__FILE__).dirname.expand_path + "views/"
+	DataMapper.finalize.auto_upgrade!
 
-def check_permission
-	if current_user.id != @weight.user
-		flash[:notice] = "You are not authorized to do that."
-		redirect '/'
+	def check_permission
+		if current_user.id != @weight.user
+			flash[:notice] = "You are not authorized to do that."
+			redirect '/'
+		end
 	end
-end
 
-
-
-get '/' do
-	if logged_in?
-		@weights = Weight.all(:user => current_user.id, :order => [:date.asc])
-		@title = "Your weights"
-		flash[:notice] = "Hi #{current_user.name}!"
-	else
-		@weights = Weight.all
-		@title = "All weights"
-		flash[:notice] = "Hi anonymous, why not log in?"
+	get '/' do
+		if logged_in?
+			@weights = Weight.all(:user => current_user.id, :order => [:date.asc])
+			@title = "Your weights"
+			flash[:notice] = "Hi #{current_user.name}!"
+		else
+			@weights = Weight.all
+			@title = "All weights"
+			flash[:notice] = "Hi anonymous, why not log in?"
+		end
+		haml :home
 	end
-	haml :home
-end
 
-post '/' do
-	if logged_in?
-		@weight = Weight.new
+	post '/' do
+		if logged_in?
+			@weight = Weight.new
+			@weight.value = params[:value]
+			@weight.date = params[:date].empty? ? Date.today : Date.parse(params[:date])
+			@weight.user = current_user.id
+			flash[:notice] = (@weight.save) ? "Weight posted successfully" : "Error: #{@weight.errors.first.first}"
+			redirect '/'
+		else
+			flash[:notice] = "You must login"
+			redirect '/login'
+		end
+	end
+
+	get '/weights/:id' do
+		@weight = Weight.get params[:id]
+		check_permission
+		@title = "Measure ##{params[:id]}"
+		haml :show_weight
+	end
+
+
+	get '/weights/:id/edit' do 
+		@weight = Weight.get params[:id]
+		check_permission
+		@title = "Edit measure ##{params[:id]}"
+		haml :edit_weight
+	end
+
+	put '/weights/:id' do
+		@weight = Weight.get params[:id]
+		check_permission
 		@weight.value = params[:value]
 		@weight.date = params[:date].empty? ? Date.today : Date.parse(params[:date])
-		@weight.user = current_user.id
-		flash[:notice] = (@weight.save) ? "Weight posted successfully" : "Error: #{@weight.errors.first.first}"
+		@weight.save!
 		redirect '/'
-	else
-		flash[:notice] = "You must login"
-		redirect '/login'
 	end
+
+	get '/weights/:id/delete' do
+		@weight = Weight.get params[:id]
+		check_permission
+		@title = "Deletion of measure ##{params[:id]}"
+		haml :delete
+	end
+
+	delete '/weights/:id' do  
+	  @weight = Weight.get params[:id]  
+	  check_permission
+	  @weight.destroy  
+	  redirect '/'  
+	end
+
+	get '/users/?*?' do
+		raise Sinatra::NotFound
+	end
+
+	register Sinatra::SinatraAuthentication
+
+	run! if app_file == $0
 end
-
-get '/weights/:id' do
-	@weight = Weight.get params[:id]
-	check_permission
-	@title = "Measure ##{params[:id]}"
-	haml :show_weight
-end
-
-
-get '/weights/:id/edit' do 
-	@weight = Weight.get params[:id]
-	check_permission
-	@title = "Edit measure ##{params[:id]}"
-	haml :edit_weight
-end
-
-put '/weights/:id' do
-	@weight = Weight.get params[:id]
-	check_permission
-	@weight.value = params[:value]
-	@weight.date = params[:date].empty? ? Date.today : Date.parse(params[:date])
-	@weight.save!
-	redirect '/'
-end
-
-get '/weights/:id/delete' do
-	@weight = Weight.get params[:id]
-	check_permission
-	@title = "Deletion of measure ##{params[:id]}"
-	haml :delete
-end
-
-delete '/weights/:id' do  
-  @weight = Weight.get params[:id]  
-  check_permission
-  @weight.destroy  
-  redirect '/'  
-end  
